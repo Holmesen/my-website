d5d321a9ef6fa427d7e46031c3a324286a60b684
holmesen <1315654996@qq.com>
2019-01-28 00:03:38 +0800

    添加悬浮图标弹出账号信息

--- src/components/cardList.vue
+++ src/components/cardList.vue
@@ -5,14 +5,14 @@
         </div>
         <div id="cardTable">
             <div>
-                <img alt="海王" src="../assets/海王.jpg">
-                <img alt="蝙蝠侠" src="../assets/蝙蝠侠.jpg">
-                <img alt="神奇女侠" src="../assets/神奇女侠.jpg">
+                <img alt="海王" title="海王" src="../assets/海王.jpg">
+                <img alt="蝙蝠侠" title="蝙蝠侠" src="../assets/蝙蝠侠.jpg">
+                <img alt="神奇女侠" title="神奇女侠" src="../assets/神奇女侠.jpg">
             </div>
             <div>
-                <img alt="闪电侠" src="../assets/闪电侠.jpg">
-                <img alt="超人" src="../assets/超人.jpg">
-                <img alt="钢骨" src="../assets/钢骨.jpg">
+                <img alt="闪电侠" title="闪电侠" src="../assets/闪电侠.jpg">
+                <img alt="超人" title="超人" src="../assets/超人.jpg">
+                <img alt="钢骨" title="钢骨" src="../assets/钢骨.jpg">
             </div>
         </div>
     </div>
@@ -35,7 +35,7 @@ export default {
 #cardTitle {
   font-size: 20px;
   text-align: center;
-  margin: 40px auto;
+  margin: 70px auto 10px auto;
   align-content: center;
 }
 #cardTable {
@@ -56,6 +56,7 @@ export default {
   height: 100%;
   width: 300px;
   margin: 0 30px;
+  box-shadow: 0 0 10px 3px grey;
 }
 #cardTable img:hover {
   opacity: 0.6;
--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,7 +1,13 @@
 <template>
     <div id="contactIcon">
         <div v-for="(item,index) in imgList" :key="index">
-            <img :src="item.flag?item.src2:item.src1" @mouseover="overIcon(index)" @mouseout="outIcon(index)"/>
+            <!-- <div id="hoverIcon" v-if="item.link=='javascript:;'">
+                <img :src="item.img"/>
+            </div> -->
+            <a id="iconA" :href="item.link">
+                <img v-if="item.link=='javascript:'" class="qrcode" :src="item.img"/>
+                <img :src="item.flag?item.src2:item.src1" @mouseover="overIcon(index)" @mouseout="outIcon(index)"/>
+            </a>
         </div>
         
     </div>
@@ -13,13 +19,52 @@ export default {
     data: function(){
         return {
             imgList:[
-                {src1:require('../assets/wechat0.png'),src2:require('../assets/wechat1.png'),flag:false},
-                {src1:require('../assets/qq0.png'),src2:require('../assets/qq1.png'),flag:false},
-                {src1:require('../assets/weibo0.png'),src2:require('../assets/weibo1.png'),flag:false},
-                {src1:require('../assets/csdn0.png'),src2:require('../assets/csdn1.png'),flag:false},
-                {src1:require('../assets/github0.png'),src2:require('../assets/github1.png'),flag:false},
-                {src1:require('../assets/ins0.png'),src2:require('../assets/ins1.png'),flag:false},
-                {src1:require('../assets/Facebook0.png'),src2:require('../assets/Facebook1.png'),flag:false},
+                {
+                    src1:require('../assets/wechat0.png'),
+                    src2:require('../assets/wechat1.png'),
+                    flag:false,
+                    link:'javascript:',
+                    img:require('../assets/微信账号二维码.jpg')
+                },
+                {
+                    src1:require('../assets/qq0.png'),
+                    src2:require('../assets/qq1.png'),
+                    flag:false,
+                    link:'javascript:',
+                    img:require('../assets/qq账号二维码.jpg')
+                },
+                {
+                    src1:require('../assets/weibo0.png'),
+                    src2:require('../assets/weibo1.png'),
+                    flag:false,
+                    link:'https://weibo.com/u/5795295209?is_all=1',
+                    // img:require('../assets/微信账号二维码.jpg')
+                },
+                {
+                    src1:require('../assets/csdn0.png'),
+                    src2:require('../assets/csdn1.png'),
+                    flag:false,
+                    link:'https://blog.csdn.net/Holmesen'
+                },
+                {
+                    src1:require('../assets/github0.png'),
+                    src2:require('../assets/github1.png'),
+                    flag:false,
+                    link:'https://github.com/Holmesen'
+                },
+                {
+                    src1:require('../assets/ins0.png'),
+                    src2:require('../assets/ins1.png'),
+                    flag:false,
+                    link:'javascript:',
+                    img:require('../assets/Ins账号.jpg')
+                },
+                {
+                    src1:require('../assets/Facebook0.png'),
+                    src2:require('../assets/Facebook1.png'),
+                    flag:false,
+                    link:'https://www.facebook.com/holmesen.lin.7'
+                },
             ]
         }
     },
@@ -48,5 +93,47 @@ export default {
     height: 30px;
     width: 30px;
     margin: auto;
+}
+#contactIcon img:hover{
+    cursor: pointer;
+}
+#hoverIcon{
+    height: auto;
+    width: 130px;
+    display: flex;
+    align-content: center;
+    position: absolute;
+}
+#hoverIcon>img{
+    height: 100%;
+    width: 100%;
+    margin: auto;
+    top: -20px;
+}
+/* 弹出图片样式 */
+#iconA {
+    position: relative;
+}
+#iconA img.qrcode {
+  position: absolute;
+  z-index: 99;
+  bottom: 50px;
+  right: -50px;
+  width: 7.5rem;
+  max-width: none;
+  height: auto;
+  transform: scale(0);
+  transform-origin: top right;
+  opacity: 0;
+  border: .3125rem solid #0085ba;
+  border-radius: .25rem;
+  -webkit-transition: all .4s ease-in-out;
+  -o-transition: all .4s ease-in-out;
+  transition: all .4s ease-in-out;
+ 
+}
+#iconA:hover img.qrcode {
+    transform: scale(1);
+    opacity: 1;
 }
 </style>
