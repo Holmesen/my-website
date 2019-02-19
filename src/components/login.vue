93f55465b8bd777f6c7bdfc30a56f40c66d5a308
holmesen <1315654996@qq.com>
2019-02-20 00:38:58 +0800

    添加一个动画库

--- src/components/login.vue
+++ src/components/login.vue
@@ -21,135 +21,191 @@
             </table>
             <p class="tips">Not registered? <span>Create an account</span></p>
             <span>用户名或密码不正确</span>
-        </div> -->
+        </div>-->
         <div id="sign_up" style="height:auto;">
             <h1>Sign Up</h1>
             <table>
                 <tr>
                     <td></td>
-                    <td><input type="text" name="username" placeholder="Username" class="input"></td>
-                    <td><img src="../assets/username_true.png" alt="" style="height:18px;width:18px; margin:auto 10px;"><span style="margin:auto 10px; font-size:15px; line-height:40px; color:#d93a49;">该用户名已被注册</span></td>
+                    <td>
+                        <input type="text" name="username" placeholder="Username" class="input">
+                    </td>
+                    <td>
+                        <img
+                            src="../assets/username_true.png"
+                            alt=""
+                            style="height:18px;width:18px; margin:auto 10px;"
+                        >
+                        <span
+                            style="margin:auto 10px; font-size:15px; line-height:40px; color:#d93a49;"
+                        >该用户名已被注册</span>
+                    </td>
                 </tr>
                 <tr>
                     <td></td>
-                    <td><input type="password" name="password" placeholder="Password" class="input"></td>
-                    <td><span style="color:#d93a49; line-height:40px; margin-left:10px; font-size:15px;">密码不得小于6位</span></td>
+                    <td>
+                        <input type="password" name="password" placeholder="Password" class="input">
+                    </td>
+                    <td>
+                        <span
+                            style="color:#d93a49; line-height:40px; margin-left:10px; font-size:15px;"
+                        >密码不得小于6位</span>
+                    </td>
                 </tr>
                 <tr>
                     <td></td>
-                    <td><input type="password" name="confirmPwd" placeholder="Confirm Password" class="input"></td>
-                    <td><span style="color:#d93a49; line-height:40px; margin-left:10px; font-size:15px;">密码不一致</span></td>
+                    <td>
+                        <input
+                            type="password"
+                            name="confirmPwd"
+                            placeholder="Confirm Password"
+                            class="input"
+                        >
+                    </td>
+                    <td>
+                        <span
+                            style="color:#d93a49; line-height:40px; margin-left:10px; font-size:15px;"
+                        >密码不一致</span>
+                    </td>
                 </tr>
                 <tr>
                     <td></td>
-                    <td><input type="submit" value="sign up" class="btn"></td>
+                    <td>
+                        <input type="submit" value="sign up" class="btn">
+                    </td>
                     <td></td>
                 </tr>
             </table>
-            <p class="tips">Already registered? <span>Log In</span></p>
+            <p class="tips">
+                Already registered?
+                <span>Log In</span>
+            </p>
         </div>
     </div>
 </template>
 
 <script>
+// import vuePose from "vue-pose";
+
 export default {
-    name: 'login'
-}
+  name: "login"
+  //   components: {
+  //     Box: vuePose.div({
+  //       visible: {
+  //         opacity: 1,
+  //         scaleY: 1,
+  //         transition: {
+  //           opacity: { ease: "easeOut", duration: 300 },
+  //           default: { ease: "linear", duration: 500 }
+  //         }
+  //       },
+  //       hidden: { opacity: 0 }
+  //     })
+  //   }
+};
 </script>
 
 <style scoped>
-#login{
-    height: -webkit-fill-available;
-    width: auto;
-    display: flex;
-    flex-flow: column;
-    margin: auto;
-    align-content: center;
-    background: #092756;
-    background-image: linear-gradient(to bottom right, #2d1b46, #192e50);
-    filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3E1D6D', endColorstr='#092756',GradientType=1 );
+#login {
+  height: -webkit-fill-available;
+  width: auto;
+  display: flex;
+  flex-flow: column;
+  margin: auto;
+  align-content: center;
+  background: #092756;
+  background-image: linear-gradient(to bottom right, #220f3d, #192e50);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#3E1D6D', endColorstr='#092756',GradientType=1 );
 }
-#log_in,#sign_up{
-    height: auto;
-    width: auto;
-    display: flex;
-    flex-flow: column;
-    margin: auto;
-    align-content: center;
+#log_in,
+#sign_up {
+  height: auto;
+  width: auto;
+  display: flex;
+  flex-flow: column;
+  margin: auto;
+  align-content: center;
 }
-#log_in>h1,#sign_up>h1{
-    font-size: 30px;
-    font-weight: bold;
-    margin: 30px auto;
-    text-align: center;
-    color: #fff; 
-    text-shadow: 0 0 10px rgba(0,0,0,0.3);
-    letter-spacing: 5px;
+#log_in > h1,
+#sign_up > h1 {
+  font-size: 30px;
+  font-weight: bold;
+  margin: 30px auto;
+  text-align: center;
+  color: #fff;
+  text-shadow: 0 0 10px rgba(0, 0, 0, 0.3);
+  letter-spacing: 5px;
 }
-#log_in>p,#sign_up>p{
-    font-size: 15px;
-    text-align: center;
-    margin-top: 10px;
-    color: #50b7c1;
+#log_in > p,
+#sign_up > p {
+  font-size: 15px;
+  text-align: center;
+  margin-top: 10px;
+  color: #50b7c1;
 }
-#log_in>span,#sign_up>span{
-    font-size: 15px;
-    font-weight: bold;
-    width: auto;
-    max-width: 80%;
-    word-wrap: break-word;
-    color: #d93a49;
-    text-align: center;
-    margin: 10px auto;
+#log_in > span,
+#sign_up > span {
+  font-size: 15px;
+  font-weight: bold;
+  width: auto;
+  max-width: 80%;
+  word-wrap: break-word;
+  color: #d93a49;
+  text-align: center;
+  margin: 10px auto;
 }
-#log_in>table,#sign_up>table{
-    border: none;
-    width: auto;
-    height: auto;
-    margin: auto;
+#log_in > table,
+#sign_up > table {
+  border: none;
+  width: auto;
+  height: auto;
+  margin: auto;
 }
-#log_in>table>tr,#sign_up>table>tr{
-    width: auto;
-    height: auto;
-    padding: 18px;
-    display: flex;
+#log_in > table > tr,
+#sign_up > table > tr {
+  width: auto;
+  height: auto;
+  padding: 18px;
+  display: flex;
 }
-#log_in>table td,#sign_up>table td{
-    width: 300px;
-    height: auto;
-    align-content: center;
-    display: flex;
-    flex-flow: row;
+#log_in > table td,
+#sign_up > table td {
+  width: 300px;
+  height: auto;
+  align-content: center;
+  display: flex;
+  flex-flow: row;
 }
-.input{
-    width: 100%;
-    height: 35px;
-    padding-left: 20px;
-    font-size: 16px;
-    line-height: 35px;
-    letter-spacing: 1px;
-    background-color: azure;
-    border-radius: 100px;
-    box-shadow: inset 0 -5px 45px rgba(100,100,100,0.2), 0 1px 1px rgba(255,255,255,0.2);
-    outline: none;
+.input {
+  width: 100%;
+  height: 35px;
+  padding-left: 20px;
+  font-size: 16px;
+  line-height: 35px;
+  letter-spacing: 1px;
+  background-color: azure;
+  border-radius: 100px;
+  box-shadow: inset 0 -5px 45px rgba(100, 100, 100, 0.2),
+    0 1px 1px rgba(255, 255, 255, 0.2);
+  outline: none;
 }
-.btn{
-    width: 180px;
-    height: 40px;
-    margin: auto;
-    font-size: 18px;
-    line-height: 40px;
-    cursor: pointer;
-    font-weight: bold;
-    color: white;
-    background-color: #64a5dc;
-    border-radius: 100px;
-    outline:none;
-    font-family: -webkit-body;
-    letter-spacing: 3px;
+.btn {
+  width: 180px;
+  height: 40px;
+  margin: auto;
+  font-size: 18px;
+  line-height: 40px;
+  cursor: pointer;
+  font-weight: bold;
+  color: white;
+  background-color: #64a5dc;
+  border-radius: 100px;
+  outline: none;
+  font-family: -webkit-body;
+  letter-spacing: 3px;
 }
-.tips>span{
-    color: #afdfe4;
-    cursor: pointer;
+.tips > span {
+  color: #afdfe4;
+  cursor: pointer;
 }
 </style>
